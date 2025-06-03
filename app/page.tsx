86fefb5e49d831eeb23a126c3d6d2a73586c6c45
Edward Güereque <edwardgnt@gmail.com>
2025-06-02 19:15:57 -0700

    Implement Companion Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import CompanionCard from '@/components/CompanionCard'
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/CTA'
+import CompanionCard from "@/components/CompanionCard";
+import React from "react";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
-      <section className='home-section'>
+      <section className="home-section">
         <CompanionCard
           id="123"
           name="Neura the Brainy Explorer"
@@ -18,26 +17,26 @@ const Page = () => {
           color="#ffda6e"
         />
         <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Mathematics"
+          duration={30}
+          color="#e5d0ff"
         />
         <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language Learning"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7ff"
         />
         <CompanionsList />
         <CTA />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
